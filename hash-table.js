0618368c3f8a3ac696da9b9bb7917b15a6763d20
Archive 15 Design <archive15design@gmail.com>
2023-10-19 12:40:06 +0200

    all tests passed, hashed table fully functional

--- hash-table.js
+++ hash-table.js
@@ -11,27 +11,91 @@ class KeyValuePair {
 class HashTable {
 
   constructor(numBuckets = 4) {
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
-    // Your code here
+    let hash256 = sha256(key).slice(0, 8);
+    let newHash = parseInt(hash256, 16);
+
+    return newHash;
+
   }
 
   hashMod(key) {
-    // Your code here
+    return this.hash(key) % this.data.length;
   }
 
   insertNoCollisions(key, value) {
-    // Your code here
+    // create hash from key
+    const hashKey = this.hashMod(key);
+
+    // check if index is already filled, if so throw error
+    if (this.data[hashKey] !== null) {
+      throw new Error('hash collision or same key/value pair already exists!');
+    } else {
+      // create key-value pair object
+    const keyPair = new KeyValuePair(key, value);
+
+    // insert key value pair at hashed index and increase count
+    this.data[hashKey] = keyPair;
+    this.count++;
+    };
+
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here
+    const hashKey = this.hashMod(key);
+
+    if (!this.data[hashKey]) {
+      // create key-value pair object
+    const keyPair = new KeyValuePair(key, value);
+
+    // insert key value pair at hashed index and increase count
+    this.data[hashKey] = keyPair;
+    this.count++;
+    }
+    else {
+      // create key-value pair object and adjust its next to current head
+      let keyPair = new KeyValuePair(key, value);
+      keyPair.next = this.data[hashKey];
+
+      // insert new key-value pair
+      this.data[hashKey] = keyPair;
+      this.count++;
+    }
   }
 
   insert(key, value) {
-    // Your code here
+    let keyPair = new KeyValuePair(key, value);
+    let hashKey = this.hashMod(key);
+
+    // handle empty buckets
+    if (!this.data[hashKey]) {
+      this.data[hashKey] = keyPair;
+      this.count++;
+      return;
+    }
+
+    // handle occupied buckets by check if new entry or updated value
+    // check if key already exists in list, if so update value and return
+    let current = this.data[hashKey];
+    while (current) {
+      if (keyPair.key === current.key) {
+        current.value = keyPair.value;
+        return;
+      }
+      current = current.next;
+    }
+
+    // if unique key, insert keyPair at head of list
+    keyPair.next = this.data[hashKey];
+    // insert new key-value pair
+    this.data[hashKey] = keyPair;
+    this.count++;
+
   }
 
 }
